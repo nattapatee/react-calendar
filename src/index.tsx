91b615ddde2621300d9a17bd653180594fce5d7a
wk-j <somnuk.wk@gmail.com>
2019-05-27 16:12:36 +0700

    Fixed format

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from "react"
 import { render } from "react-dom";
+import { CalendarInlineExample } from "./calendar/C";
+import { DateRangeType } from "office-ui-fabric-react/lib/Calendar";
 
 
 class App extends React.Component {
 
     public render() {
         return (
-            <h1>Hello</h1>
+            <div>
+                <CalendarInlineExample
+                    dateRangeType={DateRangeType.Day}
+                    showGoToToday={true}
+                    showNavigateButtons={false}
+                    autoNavigateOnSelection={true} />
+            </div>
         )
     }
 }
